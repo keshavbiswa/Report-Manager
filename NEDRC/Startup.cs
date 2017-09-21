df39c3df212c20398347745fb2a04e59cf4e3460
MyPc <MyPc@KESHAVBISWA>
2017-09-21 15:54:31 +0530

    Add project files.

---
+++ NEDRC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NEDRC.Startup))]
+namespace NEDRC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
